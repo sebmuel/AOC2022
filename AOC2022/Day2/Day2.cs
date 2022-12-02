e3e255217c65ad761ad3c34c0b5da2b6afe83ed5
sebmuel <mueller.sebastian117@protonmail.com>
2022-12-02 08:55:56 +0100

    Start Day2

--- AOC2022/Day2/Day2.cs
+++ AOC2022/Day2/Day2.cs
@@ -9,22 +9,91 @@ namespace AOC2022.D2
 {
     public class Day2 : IDay
     {
+        public string[] input { get; }
+        Game _game;
 
-        string[] IDay.input => throw new NotImplementedException();
+        public Day2()
+        {
+            input = ReadInput.GetInputLines(GetType().Name, true);
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine($"Part-1 {Part1()}");
+        }
 
         public object Part1()
         {
-            throw new NotImplementedException();
+            _game = new Game();
+
+            foreach (string line in input)
+            {
+                string[] roundGuide = line.Split(" ");
+                _game.Round(roundGuide[0], roundGuide[1]);
+            }
+            return "";
         }
 
         public object Part2()
         {
             throw new NotImplementedException();
         }
+    }
 
-        void IDay.Execute()
+    public class Game
+    {
+        Player _player;
+        Oppenent _oppenent;
+
+        public Game()
         {
-            throw new NotImplementedException();
+            _player = new Player(0);
+            _oppenent = new Oppenent();
+        }
+
+
+        public void Round(string playerShape, string opponentShape)
+        {
+            _player._shape = playerShape;
+            _oppenent._shape = opponentShape;
+        }
+
+        public bool isWin(string playerShape, string opponentShape)
+        {
+            bool win = false;
+
+            if(playerShaper)
+
+            return true;
+        }
+    }
+
+    public class Player
+    {
+        public int _score { get; set; }
+        public Dictionary<char, int> shapeScores = new Dictionary<char, int>()
+        {
+            {'Y', 2 },
+            {'X', 1 },
+            {'Z', 3 }
+        };
+
+        public string _shape { get; set; }
+
+        public Player(int score, string[] shapes)
+        {
+            _score = score;
+
+        }
+    }
+
+    public class Oppenent
+    {
+        public string _shape { get; set; }
+
+        public Oppenent()
+        {
+
         }
     }
 }
